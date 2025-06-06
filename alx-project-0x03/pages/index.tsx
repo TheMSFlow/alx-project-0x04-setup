89d1739653397eed347ba869a27d3e07cc0be978
Michael <themichaelsteve@gmail.com>
2025-06-06 21:24:37 +0100

    Import google fonts

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,20 +1,10 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function Home() {
+const Home:React.FC = () => {
   return (
     <main className="flex justify-center items-center min-h-screen w-full">
-      <h1>Home</h1>
+      <h1>Welcome to Splash App</h1>
     </main>
   );
 }
+export default Home
