9df0118814f8b7e0e0ca546b5a1ff26afa09471c
Michael <themichaelsteve@gmail.com>
2025-06-06 21:25:55 +0100

    1. Import google fonts

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -2,9 +2,9 @@
 
 const Home:React.FC = () => {
   return (
-    <main className="flex justify-center items-center min-h-screen w-full">
-      <h1>Welcome to Splash App</h1>
-    </main>
+    <div className="flex justify-center items-center min-h-screen w-full">
+      <h1> Welcome to Splash App </h1>
+    </div>
   );
 }
 export default Home
