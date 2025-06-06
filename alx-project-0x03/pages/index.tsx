0f13d1876fd47dee48a560ff893ca25f616bb0b1
Michael <themichaelsteve@gmail.com>
2025-06-06 21:20:44 +0100

    Files setup

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center min-h-screen w-full">
+      <h1>Home</h1>
+    </main>
+  );
+}
